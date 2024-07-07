d13f6d0544a896c7bf94fe041ad1de01c206a7cf
YanODZ <yanordazuwu@gmail.com>
2024-07-07 14:36:24 -0600

    Botón de canjear membresía agregado

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -87,6 +87,44 @@ export class LayoutComponent implements OnInit, AfterViewInit {
         });
       }
     });
-  }  
+  }
+  
+  canjear() {
+    Swal.fire({
+      title: 'Canjear Código',
+      html:
+        '<input id="swal-input1" class="swal2-input" type="number" placeholder="ID Usuario">' +
+        '<input id="swal-input2" class="swal2-input" maxlength="10" placeholder="Código">',
+      focusConfirm: false,
+      preConfirm: () => {
+        const userId = (document.getElementById('swal-input1') as HTMLInputElement).value;
+        const code = (document.getElementById('swal-input2') as HTMLInputElement).value;
+        if (!userId || !code || code.length !== 10) {
+          Swal.showValidationMessage('Por favor, ingresa un ID de usuario y un Code válido de 10 caracteres');
+        }
+        return { userId: Number(userId), code: code };
+      }
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.makeRedeem(result.value.userId, result.value.code);
+      }
+    });
+  }
+
+  makeRedeem(id: number, code: string) {
+    const payload = { user_id:id, code:code }
+    this.http.post<any>(`${this.apiURL}/membership/redeem/user`, payload, {
+      headers: {
+        Authorization: `Bearer ${this.currentUser.token}`
+      }
+    }).subscribe({
+      next: (response) => {
+        Swal.fire('Éxito', 'El código ha sido canjeado correctamente.', 'success');
+      },
+      error: (err) => {
+        Swal.fire('Error', 'Hubo un problema al canjear el código.', 'error');
+      }
+    });
+  }
 
 }
